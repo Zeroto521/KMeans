671267a6114248bed7775cefcc1aa8b30f948472
Zero <zeroto521@gmail.com>
2019-06-01 11:29:07 +0800

    :fire:
    IOError: [Errno 2] No such file or directory: 'watermelon4.0.csv'

--- examples/watermelon/main.py
+++ examples/watermelon/main.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import numpy as np
 
 from kmeans import KMeans
 
-data = np.loadtxt('watermelon4.0.csv', delimiter=',', skiprows=1)
+base_dir = os.path.abspath(os.path.dirname(__file__))
+filepath = os.path.join(base_dir, 'watermelon4.0.csv')
+
+data = np.loadtxt(filepath, delimiter=',', skiprows=1)
 
 model = KMeans(n_clusters=2)
 labels = model.fit(data)
